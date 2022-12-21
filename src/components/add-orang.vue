91e89e502ce3e7fe1c9ba37279edb6500b552ff6
JouskaPram <pramudita5800@gmail.com>
2022-12-21 07:54:28 +0700

    add tambah data

--- src/components/add-orang.vue
+++ src/components/add-orang.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
+    <div class="w-1/4 bg-white m-auto mt-16 shadow-md p-3 rounded-md">
+      
+
+  <div class="my-3 text-center text-gray-700 font-semibold text-xl">
+    Masukan Data
+  </div>
+  <div class="mb-6">
+    <label for="Nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+    <input type="Nama" id="Nama" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" v-model="nama " required>
+  </div>
+  <div class="mb-6">
+    <label for="kelas" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">kelas</label>
+    <input type="kelas" id="kelas" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="kelas" required>
+  </div>
+
+  <button type="submit" @click="saveData" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Simpan</button>
+  <router-link to="/" class="text-white bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 mx-5 text-center">Kembali</router-link>
+
+
+    </div>
     
-      <input type="text" v-model="nama" class="bg-slate-200">
-      <input type="text" v-model="kelas" class="bg-slate-200">
-      <button class="text-white bg-sky-500 py-2 px-5 text-lg rounded-md" @click="saveData">simpan</button>
+    
 
   </div>
 </template>
@@ -27,31 +45,23 @@ import { collection, addDoc } from "firebase/firestore";
 
     methods: {
       async saveData(){
+        this.send=true
        const docRef = await addDoc(collection(db, "orang"), {
   nama:this.nama,
   kelas: this.kelas,
 
 }
 );
-  alert("sukses")
-  this.nama=''
-  this.kelas=''
+  // alert("sukses")
+  // this.nama=''
+  // this.kelas=''
 console.log("Document written with ID: ", docRef.id);
+if(docRef) {
+        this.send = false
+        this.$router.push('/orang')
+        alert('data sukses ditambahkan')
+      }
 
-        // console.log(this.nama)
-        // console.log(this.kelas)
-      //   let obj = {
-      //     nama: this.nama,
-      //     kelas: this.kelas,
-      //   }
-      //   db.firestore.collection("orang").add(obj).then(doc=>{
-      //     alert('data berhasil di tambahkan'+doc.id)
-      //     this.nama = ''
-      //     this.kelas = ''
-      //   }).catch(e=>{
-      //     console.log(e)
-      //   })
-      // }
     },
     }
   }
--- src/components/orang.vue
+++ src/components/orang.vue
@@ -1,28 +1,52 @@
 <template>
-  <div>
-
+  <div class="mt-5">
+    <router-link to="/tambah" class="text-white font-semibold bg-sky-500 rounded-md py-2 px-5 mx-10 hover:bg-sky-400 hover:animation duration-700">Tambah Orang</router-link>
+      <table class="w-1/3 m-auto mt-10 text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr class="text-center text-slate-800 text-lg font-bold borfer-2 ">
+            <td class="py-3 px-6">Nama</td>
+            <td class="py-3 px-6">Kelas</td>
+          </tr>
+          
+        </thead>
+        <tbody>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-for="orang in orangs" :key="orang.nama">
+            <td class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white ">{{ orang.nama }}</td>
+            <td class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ orang.kelas }}</td>
+          </tr>
+    
+        </tbody>
+      </table>
   </div>
 </template>
 
 <script>
 import db from '../config/firebase.js' 
-import {doc,getDocFromCache} from 'firebase/firestore'
+import {query,getDocs,collection, orderBy} from 'firebase/firestore'
 export default {
   setup () {
     
 
-    return {}
+    },
+    data() {
+    return {
+      orangs: []
+    }
   },
-  created(){
+  created(){ 
     this.getOrang()
   },
   methods:{
     async getOrang(){
-     const docRef = doc(db, "kelas", "nama");
-const docSnap = await getDocFromCache (docRef);
-console.log(docSnap.data())
-    }
-  }
+      const querySnap = await getDocs(query(collection(db, 'orang'),orderBy("nama")));
+      console.log(querySnap)  
+      // add each doc to 'countries' array
+      querySnap.forEach((doc) => {
+        this.orangs.push(doc.data())
+        
+      })
+
+    }}
 }
 </script>
 
